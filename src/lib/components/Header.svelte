d66f69f86a0efe9b569c3d5579a8edb1fe6c5724
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2024-12-25 16:14:45 +0300

    Create `Header` component
    The `Header` component contains a title and a
    switch button to toggle the theme between "dark"
    and "light" modes.

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,12 @@
+<script>
+	import { AppBar, LightSwitch } from '@skeletonlabs/skeleton';
+</script>
+
+<AppBar background="none" padding="px-0 py-4">
+	<svelte:fragment slot="lead">
+		<h1 class="gradient-heading text-2xl font-extrabold">Mecha Agent</h1>
+	</svelte:fragment>
+	<svelte:fragment slot="trail">
+		<LightSwitch rounded="rounded-full" />
+	</svelte:fragment>
+</AppBar>
