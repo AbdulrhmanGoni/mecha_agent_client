5ef8b4519d09bac68ff1036ed844a4536d6c99c2
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2024-12-30 14:05:33 +0300

    Create `BackButton` component
    This component uses the `Button` and adds some
    style and an arrow-back icon and uses `window.history.back`
    method to create a unified "back to previous page" button.

---
+++ src/lib/components/shared/BackButton.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import Icon from '@iconify/svelte';
+	import Button from './Button.svelte';
+
+	const back = () => {
+		window.history.back();
+	};
+
+	const props: CustomButtonProps = $props();
+</script>
+
+<Button {...props} onclick={back} class="variant-filled-primary {props.class}">
+	<Icon icon="typcn:arrow-back" width="24" height="24" />
+	Back
+</Button>
