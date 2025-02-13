de6db9d209d9446e97ccbfe47182f6555e60ad13
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-02-13 21:01:13 +0300

    Set the layout of the app

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,21 @@
 <script lang="ts">
 	import '../app.css';
-	let { children } = $props();
+	import Header from '$lib/components/Header.svelte';
+	import { initializeStores, Modal, Toast } from '@skeletonlabs/skeleton';
+	import { computePosition, autoUpdate, offset, shift, flip, arrow } from '@floating-ui/dom';
+	import { storePopup } from '@skeletonlabs/skeleton';
+	initializeStores();
+
+	storePopup.set({ computePosition, autoUpdate, offset, shift, flip, arrow });
+
+	let { data, children } = $props();
 </script>
 
-{@render children()}
+<main class="container mx-auto flex min-h-screen flex-col gap-2 px-2">
+	{#if data.user}
+		<Header />
+	{/if}
+	{@render children()}
+	<Toast />
+	<Modal />
+</main>
