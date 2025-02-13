fb752868aa152063f15f903aeb322b809a6b577e
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-02-13 21:03:03 +0300

    Declare agents-related types
    Create `src/agents.d.ts` file and declare inside it
    `Agent` type and `Dataset` type.

---
+++ src/agents.d.ts
@@ -0,0 +1,21 @@
+type Dataset = {
+    id: string;
+    title: string;
+    description: string;
+    agentId: string;
+    status: "processing" | "processed" | "unprocessed";
+    createdAt: string;
+}
+
+type Agent = {
+    id: string;
+    agentName: string;
+    description: string;
+    avatar?: string;
+    systemInstructions?: string;
+    dataset?: Dataset;
+    createdAt: string;
+    dontKnowResponse?: string;
+    responseSyntax?: "markdown"
+    greetingMessage?: string;
+};
