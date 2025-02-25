a41cb1138878c2d9b2ce2beabae957001a34d06b
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-02-25 15:31:12 +0300

    Create the component that displays "no agents" message

---
+++ src/lib/components/agents/NoAgentsMessage.svelte
@@ -0,0 +1,11 @@
+<script>
+	import Icon from '@iconify/svelte';
+</script>
+
+<div class="flex flex-1 flex-col items-center justify-center gap-3 px-4 py-20">
+	<Icon icon="mdi:robot-off-outline" width="100" height="100" />
+	<h2 class="h2 font-bold">You dont have agents yet</h2>
+	<p class="text-center sm:text-lg">
+		You can click on the button below to go to agents creation form and create your first agent
+	</p>
+</div>
