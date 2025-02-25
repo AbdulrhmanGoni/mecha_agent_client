e5bd7007e805e817582ceb2893788a9ffcb6d7b0
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-02-25 15:29:52 +0300

    Create agents grid component

---
+++ src/lib/components/agents/AgentsGrid.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import AgentCard from '$lib/components/agents/AgentCard.svelte';
+	import { agentsState } from '../../../stores/agents.svelte';
+</script>
+
+<div class="grid gap-3 sm:grid-cols-2 lg:grid-cols-3">
+	{#each agentsState.agents as agent}
+		<AgentCard {agent} />
+	{/each}
+</div>
