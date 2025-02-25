034dcaa7aa533e4cab9d4ec6918761c53c2a2ea4
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-02-25 15:00:54 +0300

    Create the button that takes users to chat with an agent page

---
+++ src/lib/components/chat/ChatWithAgentButton.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { agentsState } from '../../../stores/agents.svelte';
+	import Button from '../shared/Button.svelte';
+	import Icon from '@iconify/svelte';
+	import { goto } from '$app/navigation';
+
+	const { agent }: { agent: Agent } = $props();
+</script>
+
+<Button
+	disabled={agentsState.actionInProgress}
+	size="sm"
+	class={`variant-filled-primary ${agentsState.actionInProgress ? 'cursor-not-allowed' : ''}`}
+	onclick={(e) => {
+		e.stopPropagation();
+		goto(`/agents/${agent.id}/chat`);
+	}}
+>
+	<Icon icon="ri:chat-ai-line" width="22" height="22" />
+</Button>
