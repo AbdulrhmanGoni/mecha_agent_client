c112b5a02a05bcdd540a330a07efb797ae3b35ea
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-02-25 14:57:51 +0300

    Create agents grid error state component

---
+++ src/lib/components/agents/AgentsGridError.svelte
@@ -0,0 +1,13 @@
+<script>
+	import { agentsState } from '../../../stores/agents.svelte';
+	import ErrorPage from '../shared/ErrorPage.svelte';
+
+	const { errorMessage } = $props();
+</script>
+
+<ErrorPage
+	title="Failed to fetch the Agents"
+	errorMessage={errorMessage || 'Something wrong happened while fetching your Agents'}
+	iconName="emojione:sad-but-relieved-face"
+	retryFn={agentsState.refetchAgents}
+/>
