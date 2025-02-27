522e65c937aead4c213489d4b768eb6c97c6a4ab
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-02-28 00:31:30 +0300

    Create `AgentFormLoading` component

---
+++ src/lib/components/agents/AgentFormLoading.svelte
@@ -0,0 +1,42 @@
+<div class="space-y-4">
+	<div class="flex items-center justify-between gap-2">
+		<div class="placeholder h-7 w-48 rounded-md sm:h-10"></div>
+		<div class="placeholder h-10 w-20 rounded-md"></div>
+	</div>
+	<div class="card flex flex-col gap-4 p-4">
+		<div class="flex flex-col gap-2 sm:flex-row">
+			<div class="flex-1 space-y-1.5">
+				<div class="placeholder h-5 w-32 rounded-md"></div>
+				<div class="placeholder h-11 w-full rounded-md"></div>
+			</div>
+			<div class="flex flex-1 items-end gap-2">
+				<div class="flex-1 space-y-1.5">
+					<div class="placeholder h-5 w-20 rounded-md"></div>
+					<div class="placeholder h-11 w-full rounded-md"></div>
+				</div>
+				<div class="placeholder-circle size-16"></div>
+			</div>
+		</div>
+		<div class="flex-1 space-y-1.5">
+			<div class="placeholder h-5 w-32 rounded-md"></div>
+			<div class="placeholder h-24 w-full rounded-md"></div>
+		</div>
+		<div class="flex-1 space-y-1.5">
+			<div class="placeholder h-5 w-32 rounded-md"></div>
+			<div class="placeholder h-40 w-full rounded-md"></div>
+		</div>
+		<div class="flex-1 space-y-1.5">
+			<div class="placeholder h-5 w-48 rounded-md"></div>
+			<div class="placeholder h-16 w-full rounded-md"></div>
+		</div>
+		<div class="flex-1 space-y-1.5">
+			<div class="placeholder h-5 w-32 rounded-md"></div>
+			<div class="placeholder h-11 w-full rounded-md"></div>
+		</div>
+		<div class="flex-1 space-y-1.5">
+			<div class="placeholder h-5 w-32 rounded-md"></div>
+			<div class="placeholder h-11 w-full rounded-md"></div>
+		</div>
+		<div class="placeholder ml-auto h-11 w-32 rounded-md"></div>
+	</div>
+</div>
