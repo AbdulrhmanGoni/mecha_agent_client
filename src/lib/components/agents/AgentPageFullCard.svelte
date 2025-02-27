34a3c1b4a56890def35ef7e2b6c0922bbebe8cc9
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-02-28 00:26:26 +0300

    Create `AgentPageFullCard` component

---
+++ src/lib/components/agents/AgentPageFullCard.svelte
@@ -0,0 +1,66 @@
+<script lang="ts">
+	import { Avatar } from '@skeletonlabs/skeleton';
+	import AgentFallbackAvatar from './AgentFallbackAvatar.svelte';
+	import AgentFieldDetail from './AgentFieldDetail.svelte';
+	import DeleteAgentButton from './DeleteAgentButton.svelte';
+	import EditAgentButton from './EditAgentButton.svelte';
+	import ChatWithAgentButton from '../chat/ChatWithAgentButton.svelte';
+	import mediaURL from '$lib/functions/mediaURL';
+	import { goto } from '$app/navigation';
+
+	const { agent }: { agent: Agent } = $props();
+</script>
+
+<div class="flex flex-1 flex-col">
+	<div
+		class="variant-gradient-secondary-tertiary relative h-40 w-full rounded-t-lg bg-gradient-to-br sm:h-64"
+	></div>
+	<div
+		class="relative mx-auto -mt-20 size-36 rounded-full border-4 border-solid border-white sm:-mt-36 sm:size-60"
+	>
+		<Avatar width="w-full" src={mediaURL.agentsAvatars(agent.avatar)}>
+			<AgentFallbackAvatar />
+		</Avatar>
+	</div>
+	<div class="p-3 text-center">
+		<h2 class="h2 line-clamp-2 font-bold">{agent.agentName}</h2>
+		<p class="line-clamp-3 text-lg">{agent.description}</p>
+	</div>
+	<div class="mx-3 flex items-center justify-center gap-2 pb-3">
+		<ChatWithAgentButton {agent} />
+		<EditAgentButton {agent} />
+		<DeleteAgentButton {agent} onSuccess={() => goto('/agents')} />
+	</div>
+	<hr class="!border-t-1 my-3" />
+	<div class="my-3 grid gap-4 md:grid-cols-2">
+		<AgentFieldDetail
+			title="System Instructions"
+			value={agent.systemInstructions}
+			iconName="ic:outline-integration-instructions"
+			description="Additional system instructions to customize the behavior of the agent"
+			noValueMessage="No additional system instructions"
+		/>
+		<AgentFieldDetail
+			title="Don't Know Response"
+			value={agent.dontKnowResponse}
+			iconName="tabler:message-question"
+			description="What should the agent respond if it doesn't find a suitable response?"
+			noValueMessage="No custom don't know response, The default will be used"
+		/>
+		<AgentFieldDetail
+			title="Greeting Message"
+			value={agent.greetingMessage}
+			iconName="mdi:human-greeting-variant"
+			description="How does your agent greet users?"
+			noValueMessage="No custom greeting message, The default will be used"
+		/>
+		<AgentFieldDetail
+			title="Response Syntax"
+			value={agent.responseSyntax}
+			iconName="fluent:code-text-16-filled"
+			noValueMessage="No perfered response syntax, a normal text will be used"
+			description="The syntax used to format the response of the agent"
+			valueStyle={agent.responseSyntax ? 'variant-soft-primary w-fit rounded-md py-1.5 px-3' : ''}
+		/>
+	</div>
+</div>
