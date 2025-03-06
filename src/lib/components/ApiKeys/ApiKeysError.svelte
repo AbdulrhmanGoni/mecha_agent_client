ea4e85b951e657a3d13b285ffbe86c8e66a395dc
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-03-06 14:38:02 +0300

    Create `ApiKeysError` component for api keys table

---
+++ src/lib/components/ApiKeys/ApiKeysError.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { apiKeysState } from '../../../stores/apiKeys.svelte';
+	import Alert from '../shared/Alert.svelte';
+
+	const props: { error: string } = $props();
+</script>
+
+<div
+	class="absolute left-0 top-0 flex h-full w-full items-center justify-center text-center text-lg font-semibold"
+>
+	<Alert
+		variant="error"
+		title="Error"
+		message={props.error}
+		actionIconName="stash:arrow-retry"
+		actionFunction={apiKeysState.refetchApiKeys}
+	/>
+</div>
