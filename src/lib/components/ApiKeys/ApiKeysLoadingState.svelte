371e8d3ce0ce4af263807f20f18e40e04e5f64bf
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-03-06 14:37:36 +0300

    Create `ApiKeysLoadingState` component for api keys table

---
+++ src/lib/components/ApiKeys/ApiKeysLoadingState.svelte
@@ -0,0 +1,9 @@
+<script>
+	import { ProgressRadial } from '@skeletonlabs/skeleton';
+</script>
+
+<div
+	class="absolute left-0 top-0 flex h-full w-full items-center justify-center rounded-none text-center text-lg font-semibold"
+>
+	<ProgressRadial width="w-10" />
+</div>
