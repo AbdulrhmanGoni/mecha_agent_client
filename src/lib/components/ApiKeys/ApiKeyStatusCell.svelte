a503ae69871ad4e05b1e529eb426eb656486e75e
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-03-06 14:35:19 +0300

    Create `ApiKeyStatusCell` component for api keys table

---
+++ src/lib/components/ApiKeys/ApiKeyStatusCell.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	const { apiKeyRow }: { apiKeyRow: ApiKeyRow } = $props();
+
+	const badgeVariantMap: Record<ApiKeyRow['status'], string> = {
+		Active: 'variant-filled-success',
+		Inactive: 'variant-filled-warning'
+	};
+</script>
+
+<span class={`${badgeVariantMap[apiKeyRow.status] || 'variant-filled'} badge`}>
+	{apiKeyRow.status}
+</span>
