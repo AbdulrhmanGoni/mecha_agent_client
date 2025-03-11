9bb92ead640f6dfde97328b2c0a6abb1da58b606
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-03-11 23:58:28 +0300

    Declare all chat-related types in `src/chats.d.ts` file

---
+++ src/chats.d.ts
@@ -0,0 +1,33 @@
+type ChatMessage = {
+    role: "user" | "agent";
+    content: string;
+    isGenerating?: boolean;
+    error?: boolean;
+}
+
+type ChatHistory = {
+    id: string;
+    agentId: string;
+    title: string;
+    messages: ChatMessage[];
+    startedAt: string;
+}
+
+type OpenedChatState = {
+    chatFeed: ChatMessage[];
+    isGenerating: boolean;
+    isFetching: boolean;
+    isFetched: boolean;
+    error?: string;
+    reGenerate: () => void;
+    currentPrompt: string;
+}
+
+type ChatsState = {
+    chatsHistory: ChatHistory[];
+    isFetching: boolean;
+    isFetched: boolean;
+    actionInProgress: boolean,
+    reFetched: () => void;
+    error?: string;
+}
