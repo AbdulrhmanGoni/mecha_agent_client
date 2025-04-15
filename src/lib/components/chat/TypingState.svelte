5afea60677ed22ca01f71d6dbe65c8cd827d2db2
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-04-16 01:40:33 +0300

    Create `TypingState` component that displays "generating agent response" state

---
+++ src/lib/components/chat/TypingState.svelte
@@ -0,0 +1,11 @@
+<div class="mt-1.5 flex w-fit items-center justify-center space-x-1.5">
+	<div
+		class="h-2.5 w-2.5 animate-bounce rounded-full bg-primary-500 [animation-delay:-0.3s] [animation-duration:0.5s]"
+	></div>
+	<div
+		class="h-2.5 w-2.5 animate-bounce rounded-full bg-primary-500 [animation-delay:-0.6s] [animation-duration:0.5s]"
+	></div>
+	<div
+		class="h-2.5 w-2.5 animate-bounce rounded-full bg-primary-500 [animation-duration:0.5s]"
+	></div>
+</div>
