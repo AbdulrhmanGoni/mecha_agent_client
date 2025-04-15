a7fc5f6e73847e9afeeaace874a81c92dacf2131
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-04-15 21:33:05 +0300

    Create `UserPrompt` component
    Which is the component that displays the user part in
    chatting with agent page.

---
+++ src/lib/components/chat/UserPrompt.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import Icon from '@iconify/svelte';
+	import { Avatar } from '@skeletonlabs/skeleton';
+	let { userMessage, user }: { userMessage: ChatMessage; user: User } = $props();
+</script>
+
+<div class="grid grid-cols-[auto_1fr] gap-2">
+	<Avatar src={user.avatar} width="w-12">
+		<Icon icon="arcticons:super-status-bar" width="48" height="48" />
+	</Avatar>
+	<div class="card variant-soft space-y-2 rounded-tl-none p-4">
+		<header class="flex items-center justify-between">
+			<p class="font-bold">{user.name}</p>
+		</header>
+		<p>{userMessage.content}</p>
+	</div>
+</div>
