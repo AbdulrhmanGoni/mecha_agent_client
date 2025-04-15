1f62bf81363daf7f3536fb68b33d5f30067ee6c8
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-04-16 02:12:34 +0300

    Create the proxy of `DELETE /api/chats/chatId` api endpoint

--- src/routes/api/chats/[chatId]/+server.ts
+++ src/routes/api/chats/[chatId]/+server.ts
@@ -20,3 +20,12 @@ export async function GET({ cookies, url }) {
     return new Response(response.body, { status: response.status });
 }
 
+export async function DELETE({ cookies, url }) {
+    const response = await serverFetchAPI({
+        path: url.pathname + url.search,
+        cookies,
+        method: "DELETE"
+    })
+
+    return new Response(response.body, { status: response.status });
+}
