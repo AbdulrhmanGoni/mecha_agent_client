dc24fb6541275935fff56ef6c9ef0de83e82be99
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-04-15 21:17:43 +0300

    Create `AgentChattingPageLoading` component

---
+++ src/lib/components/chat/AgentChattingPageLoading.svelte
@@ -0,0 +1,13 @@
+<div class="flex gap-2">
+	<div class="placeholder h-9 w-20 animate-pulse"></div>
+	<div class="placeholder h-9 w-32 animate-pulse"></div>
+</div>
+<div class="flex flex-1 flex-col gap-5">
+	<div
+		class="mx-auto flex w-full max-w-[1000px] flex-1 flex-col items-center justify-center gap-5 sm:gap-8"
+	>
+		<div></div>
+		<div class="placeholder h-10 w-4/5 animate-pulse sm:w-1/2"></div>
+		<div class="placeholder h-11 w-full animate-pulse"></div>
+	</div>
+</div>
