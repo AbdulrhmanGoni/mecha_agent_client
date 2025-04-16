0975e8ffa78d008186d9ce8bfb5bca35297888de
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-04-16 13:22:36 +0300

    Create `ChatMessagesLoading` component

---
+++ src/lib/components/chat/ChatMessagesLoading.svelte
@@ -0,0 +1,19 @@
+<div class="flex w-full flex-1 flex-col gap-2.5 transition-all">
+	<div class="grid grid-cols-[auto_1fr] gap-2">
+		<div class="placeholder size-12 animate-pulse rounded-full"></div>
+		<div class="placeholder h-20 w-full animate-pulse"></div>
+	</div>
+	<div class="grid grid-cols-[1fr_auto] gap-2">
+		<div class="placeholder h-36 w-full animate-pulse"></div>
+		<div class="placeholder size-12 animate-pulse rounded-full"></div>
+	</div>
+	<div class="grid grid-cols-[auto_1fr] gap-2">
+		<div class="placeholder size-12 animate-pulse rounded-full"></div>
+		<div class="placeholder h-20 w-full animate-pulse"></div>
+	</div>
+	<div class="grid grid-cols-[1fr_auto] gap-2">
+		<div class="placeholder h-28 w-full animate-pulse"></div>
+		<div class="placeholder size-12 animate-pulse rounded-full"></div>
+	</div>
+</div>
+<div class="placeholder h-11 w-full animate-pulse"></div>
