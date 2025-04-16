fe36d11c7e1bd6d55a09bdf0c012ab9c80bcdc24
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-04-16 13:30:06 +0300

    Create a drawer and push the chats history list in it

---
+++ src/lib/components/chat/ChatsHistoryDrawer.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import Icon from '@iconify/svelte';
+	import Button from '../shared/Button.svelte';
+	import { Drawer, getDrawerStore } from '@skeletonlabs/skeleton';
+	import ChatsHistory from './ChatsHistory.svelte';
+
+	const drawerStore = getDrawerStore();
+
+	function openDrawer() {
+		drawerStore.open({
+			width: 'w-11/12 sm:w-[400px]'
+		});
+	}
+</script>
+
+<div>
+	<Button onclick={openDrawer} size="sm" class="variant-filled-secondary flex w-fit">
+		<Icon icon="material-symbols:history-rounded" width="24" height="24" />
+		Chats Histories
+	</Button>
+</div>
+<Drawer describedby="chats-histories">
+	<ChatsHistory closeDrawer={drawerStore.close} />
+</Drawer>
