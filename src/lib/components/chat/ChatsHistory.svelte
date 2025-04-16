a1d04f4554da5734c2f65a8000a0d242ed0eefd7
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-04-16 13:28:44 +0300

    Create `ChatsHistory` component
    Which fetches and renders a list of the chats that the user
    had with and agent.

---
+++ src/lib/components/chat/ChatsHistory.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+	import Icon from '@iconify/svelte';
+	import { onMount } from 'svelte';
+	import { page } from '$app/stores';
+	import { chatsHistoryState, fetchAgentChats } from '../../../stores/chatStore.svelte';
+	import Button from '../shared/Button.svelte';
+	import { goto } from '$app/navigation';
+	import ChatHistoryLoading from './ChatHistoryLoading.svelte';
+	import Alert from '../shared/Alert.svelte';
+	import ChatHistoryItem from './ChatHistoryItem.svelte';
+
+	onMount(() => {
+		fetchAgentChats($page.params.agentId);
+	});
+
+	const { closeDrawer }: { closeDrawer: () => void } = $props();
+</script>
+
+<div class="flex h-full min-w-52 flex-col gap-3 p-3">
+	<h4 class="h4 flex items-center gap-2 font-bold">
+		<Icon icon="material-symbols:history-rounded" width="24" height="24" />
+		Chats Histories
+	</h4>
+	<ul class="list flex-1 space-y-1.5 overflow-auto">
+		{#if chatsHistoryState.isFetching}
+			<ChatHistoryLoading />
+		{:else if chatsHistoryState.error}
+			<Alert title="Error" message={chatsHistoryState.error} variant="error" />
+		{:else if chatsHistoryState.isFetched}
+			{#if chatsHistoryState.chatsHistory.length}
+				{#each chatsHistoryState.chatsHistory as chatHistory}
+					<ChatHistoryItem {chatHistory} {closeDrawer} />
+				{/each}
+			{:else}
+				<p>No chats history</p>
+			{/if}
+		{/if}
+	</ul>
+	<Button
+		class="variant-filled-secondary"
+		onclick={() => {
+			goto(`/agents/${$page.params.agentId}/chat/new`, { state: { isNewChat: true } });
+			closeDrawer();
+		}}
+	>
+		<Icon icon="material-symbols:chat-add-on-outline-rounded" width="24" height="24" />
+		New Chat
+	</Button>
+</div>
