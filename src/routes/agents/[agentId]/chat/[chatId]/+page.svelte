52e2e9d5affd12bf867eb5d11199f867bf9bf887
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-04-17 15:36:52 +0300

    Create chatting with agent page and make it protected

---
+++ src/routes/agents/[agentId]/chat/[chatId]/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import { agentPageState, fetchAgentState } from '../../../../../stores/agentPage.svelte';
+	import AgentPageError from '$lib/components/agents/AgentPageError.svelte';
+	import AgentChattingPageLoading from '$lib/components/chat/AgentChattingPageLoading.svelte';
+	import AgentChatting from '$lib/components/chat/AgentChatting.svelte';
+
+	fetchAgentState($page.params.agentId);
+</script>
+
+{#if agentPageState.isFetching}
+	<AgentChattingPageLoading />
+{:else if agentPageState.error}
+	<AgentPageError errorMessage={agentPageState.error} />
+{:else if agentPageState.agent}
+	<AgentChatting user={$page.data.user} agent={agentPageState.agent} chatId={$page.params.chatId} />
+{/if}
