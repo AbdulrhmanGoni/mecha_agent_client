68939bf3c2e7ad6df4d1584899dc44540aa216cb
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-04-18 23:49:17 +0300

    Make `routesProtector` function fetch the full user's data
    Make `routesProtector` function fetch the full user's data instead
    of only user's session data; And then edit all server functions that
    use `routesProtector` to pass the `cookies` object.
    
    Also in `src/app.d.ts` global types file, Set the type of `user` object
    that is returned from `routesProtector` function which is used in
    all protected routes.

--- src/lib/functions/routesProtector.ts
+++ src/lib/functions/routesProtector.ts
@@ -1,8 +1,15 @@
-import { redirect } from "@sveltejs/kit";
+import { redirect, type Cookies } from "@sveltejs/kit";
+import serverFetchAPI from "./serverFetchAPI";
 
 const publicRoutes = ["/sign-in"];
 
-export default async function routesProtector({ locals, url }: { locals: App.Locals, url: URL }) {
+type RoutesProtectorParams = {
+    locals: App.Locals,
+    url: URL,
+    cookies?: Cookies
+}
+
+export default async function routesProtector({ locals, url, cookies }: RoutesProtectorParams) {
     const session = await locals.auth();
 
     if (session) {
@@ -10,7 +17,15 @@ export default async function routesProtector({ locals, url }: { locals: App.Loc
             redirect(307, '/');
         }
 
-        return session;
+        const res = await serverFetchAPI({ path: '/api/users', cookies })
+            .then((res) => res.json())
+
+        return {
+            user: {
+                ...session.user,
+                ...res.result
+            }
+        };
     }
 
     if (!publicRoutes.includes(url.pathname)) {
--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,6 +1,6 @@
 import routesProtector from "$lib/functions/routesProtector";
 import type { LayoutServerLoad } from "./$types";
 
-export const load: LayoutServerLoad = async ({ locals, url }) => {
-    return routesProtector({ locals, url })
+export const load: LayoutServerLoad = async ({ locals, url, cookies }) => {
+    return routesProtector({ locals, url, cookies })
 };
