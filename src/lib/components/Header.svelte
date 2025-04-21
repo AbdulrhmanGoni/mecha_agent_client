6ad5f83c9f7a2804b9c4466aca1a1b1b18be4872
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-04-21 13:12:06 +0300

    Add the logo of the project to the header

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,11 +1,14 @@
 <script>
-	import { AppBar, LightSwitch } from '@skeletonlabs/skeleton';
+	import { AppBar, Avatar, LightSwitch } from '@skeletonlabs/skeleton';
 	import NavigationMenu from './NavigationMenu.svelte';
 </script>
 
 <AppBar background="none" padding="px-0 py-4">
 	<svelte:fragment slot="lead">
-		<h1 class="gradient-heading text-2xl font-extrabold">Mecha Agent</h1>
+		<div class="flex items-center gap-3">
+			<Avatar width="w-10" src="/mecha_agent_logo.webp">MA</Avatar>
+			<h1 class="gradient-heading text-2xl font-extrabold">Mecha Agent</h1>
+		</div>
 	</svelte:fragment>
 	<svelte:fragment slot="trail">
 		<LightSwitch rounded="rounded-full" />
