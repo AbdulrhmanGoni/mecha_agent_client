eb0ca58bfec687bbd4be5920bc35c46f776ba5e0
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-05-06 14:52:15 +0300

    Set up the routes protector function in `/sign-in` route
    Why?, Because the routes protector function will redirect the signed in
    users to the home page if they visited `/sign-in` page.

--- src/routes/sign-in/+page.server.ts
+++ src/routes/sign-in/+page.server.ts
@@ -1,3 +1,10 @@
 import { signIn } from "../../auth"
 import type { Actions } from "./$types"
+import routesProtector from "$lib/functions/routesProtector";
+import type { PageServerLoad } from "./$types";
+
 export const actions: Actions = { default: signIn }
+
+export const load: PageServerLoad = async ({ locals, url }) => {
+    return routesProtector({ locals, url })
+};
