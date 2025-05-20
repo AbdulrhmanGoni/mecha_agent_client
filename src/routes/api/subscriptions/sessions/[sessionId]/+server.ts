0f88fc5fe5e1260daa90abdc9456f68e4afc2623
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-05-20 23:40:13 +0300

    Create the proxy of `GET /api/subscriptions/sessions/:id` endpoint

---
+++ src/routes/api/subscriptions/sessions/[sessionId]/+server.ts
@@ -0,0 +1,7 @@
+import serverFetchAPI from '$lib/functions/serverFetchAPI';
+
+export async function GET({ cookies, url }) {
+    const response = await serverFetchAPI({ path: url.pathname + url.search, cookies })
+
+    return new Response(response.body, { status: response.status });
+}
