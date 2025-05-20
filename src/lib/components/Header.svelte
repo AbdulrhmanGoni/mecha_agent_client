e3f388de7d48b033c558e85560bc9f5e56aaf7d1
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-05-20 23:42:54 +0300

    Add "Upgrade 🚀" button to the header for unsubscribed users

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -2,6 +2,8 @@
 	import { AppBar, Avatar, LightSwitch } from '@skeletonlabs/skeleton';
 	import NavigationMenu from './NavigationMenu.svelte';
 	import { userDataState } from '../../stores/userData.svelte';
+	import Button from './shared/Button.svelte';
+	import { goto } from '$app/navigation';
 </script>
 
 <AppBar background="none" padding="px-0 py-4">
@@ -13,6 +15,11 @@
 	</svelte:fragment>
 	<svelte:fragment slot="trail">
 		<LightSwitch rounded="rounded-full" />
+		{#if userDataState.user?.currentPlan === 'Free'}
+			<Button size="sm" class="variant-ghost-primary" onclick={() => goto('/plans')}>
+				Upgrade 🚀
+			</Button>
+		{/if}
 		{#if userDataState.isFetching}
 			<div class="placeholder h-11 w-11 rounded-full"></div>
 		{:else if userDataState.user}
