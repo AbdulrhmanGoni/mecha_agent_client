9313bbcdddb1af999dd3601a7d7b2fa2021dc278
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-06-08 23:11:00 +0300

    Style all icons with `flex-shrink: 0` style
    The purpose of this style is to not allow the icons to
    get shrunk when the width of their parant elements
    get smaller.

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -9,8 +9,8 @@
 <AppBar background="none" padding="px-0 py-4">
 	<svelte:fragment slot="lead">
 		<div class="flex items-center gap-3">
-			<Avatar width="w-10" src="/mecha_agent_logo.webp">MA</Avatar>
-			<h1 class="gradient-heading text-2xl font-extrabold">Mecha Agent</h1>
+			<Avatar width="w-10 shrink-0" src="/mecha_agent_logo.webp">MA</Avatar>
+			<h1 class="gradient-heading text-xl font-extrabold sm:text-2xl">Mecha Agent</h1>
 		</div>
 	</svelte:fragment>
 	<svelte:fragment slot="trail">
