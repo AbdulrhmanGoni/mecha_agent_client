a2846e1356c5d957643be006494c4f91c67a330b
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-06-14 01:24:43 +0300

    Create a file to manage datasets state in it
    First add `GET` route proxy handler to `/api/datasets/+server.ts`
    file to fetch the datasets.
    
    And then create `datasets.svelte.ts` file to fetch and  manage datasets
    state in it.

--- src/routes/api/datasets/+server.ts
+++ src/routes/api/datasets/+server.ts
@@ -1,5 +1,11 @@
 import serverFetchAPI from '$lib/functions/serverFetchAPI';
 
+export async function GET({ cookies }) {
+    const response = await serverFetchAPI({ path: "/api/datasets", cookies })
+
+    return new Response(response.body, { status: response.status });
+}
+
 export async function POST({ request, cookies, url }) {
     const formData = await request.formData();
 
---
+++ src/stores/datasets.svelte.ts
@@ -0,0 +1,34 @@
+import clientFetchAPI from "$lib/functions/clientFetchAPI";
+
+type DatasetsState = {
+    datasets: Dataset[];
+    fetching: boolean;
+    fetched: boolean;
+    error: string;
+}
+
+const datasetsState = $state<DatasetsState>({
+    datasets: [],
+    fetching: false,
+    fetched: false,
+    error: "",
+});
+
+function fetchDatasets() {
+    datasetsState.fetching = true
+    clientFetchAPI<Dataset[]>({ path: "/api/datasets" })
+        .then((datasets) => {
+            datasetsState.datasets = datasets;
+            datasetsState.fetched = true;
+            datasetsState.error = "";
+        })
+        .catch((error) => {
+            datasetsState.error = error;
+            datasetsState.fetched = false;
+        })
+        .finally(() => {
+            datasetsState.fetching = false
+        })
+}
+
+export { datasetsState, fetchDatasets }
