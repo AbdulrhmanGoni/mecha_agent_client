9b831d71835b25c4990a83c4729a178216005a88
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-06-17 14:19:45 +0300

    Build the first section in the home page (Dashboard)
    - Create `InfoBox` component and use it to display some user's
    account info in the dashboard.
    
    - Display loading skeleton ui while user's data are loading.
    
    - Handle displaying error component when something wrong
    happen while fetching user's data.

---
+++ src/lib/components/dashboard/InfoBox.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import { goto } from '$app/navigation';
+
+	type InfoBoxProps = {
+		title: string;
+		value: number;
+		icon: string;
+		goTo?: string;
+	};
+	const { title, value, icon, goTo }: InfoBoxProps = $props();
+
+	function navigate() {
+		goTo && goto(goTo);
+	}
+</script>
+
+<!-- svelte-ignore a11y_click_events_have_key_events, a11y_no_static_element_interactions -->
+<div
+	class="card flex h-full flex-col gap-4 p-4 {goTo ? 'cursor-pointer' : ''}"
+	onclick={goTo ? navigate : undefined}
+>
+	<p class="font-medium">{title}</p>
+	<div class="flex justify-between gap-2">
+		<p class="text-3xl font-bold">{value}</p>
+		<span class="iconify size-9 {icon}"></span>
+	</div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,58 @@
 <script lang="ts">
+	import InfoBox from '$lib/components/dashboard/InfoBox.svelte';
+	import ErrorPage from '$lib/components/shared/ErrorPage.svelte';
+	import { userDataState } from '../stores/userData.svelte';
 </script>
 
-<div class="space-y-4 py-4 sm:py-16">Home page</div>
+{#if userDataState.error}
+	<ErrorPage
+		iconName="hugeicons--rss-error"
+		errorMessage={userDataState.error}
+		title="Failed to fetch user data"
+		hideBackButton
+		retryFn={() => {
+			window.location.reload();
+		}}
+	/>
+{:else}
+	<div class="grid grid-cols-8 gap-3">
+		{#if userDataState.isFetching}
+			<div class="placeholder col-span-4 h-28 rounded-lg md:col-span-2"></div>
+			<div class="placeholder col-span-4 h-28 rounded-lg md:col-span-2"></div>
+			<div class="placeholder col-span-4 h-28 rounded-lg md:col-span-2"></div>
+			<div class="placeholder col-span-4 h-28 rounded-lg md:col-span-2"></div>
+		{:else if userDataState.user}
+			<div class="col-span-4 md:col-span-2">
+				<InfoBox
+					title="Agents"
+					value={userDataState.user.agentsCount}
+					icon="hugeicons--bot text-tertiary-500"
+					goTo="/agents"
+				/>
+			</div>
+			<div class="col-span-4 md:col-span-2">
+				<InfoBox
+					title="Published Agents"
+					value={userDataState.user.publishedAgents}
+					icon="hugeicons--internet text-secondary-500"
+				/>
+			</div>
+			<div class="col-span-4 md:col-span-2">
+				<InfoBox
+					title="Datasets"
+					value={userDataState.user.datasetsCount}
+					icon="hugeicons--database text-success-500"
+					goTo="/datasets"
+				/>
+			</div>
+			<div class="col-span-4 md:col-span-2">
+				<InfoBox
+					title="API Keys"
+					value={userDataState.user.apiKeysCount}
+					icon="hugeicons--key-01 text-warning-500"
+					goTo="/api-keys"
+				/>
+			</div>
+		{/if}
+	</div>
+{/if}
