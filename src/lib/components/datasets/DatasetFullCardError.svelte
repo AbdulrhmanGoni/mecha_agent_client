c85cdb41a1bb0a9ac4bc9ca0337a7d4df420716f
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-06-18 15:25:17 +0300

    Create a "loading and error state" for `DatasetFullCard` component
    - Create `DatasetFullCardError` component to be displayed in place of
    `DatasetFullCard` component in error state.
    
    - Create `DatasetFullCardLoading` component to be displayed in place
    of `DatasetFullCard` component in loading state.

---
+++ src/lib/components/datasets/DatasetFullCardError.svelte
@@ -0,0 +1,22 @@
+<script>
+	import { page } from '$app/stores';
+	import { datasetPageState, fetchDataset } from '../../../stores/datasetPage.svelte';
+	import Button from '../shared/Button.svelte';
+</script>
+
+<div class="flex w-full flex-col gap-3 px-3 py-4">
+	<div class="flex items-center gap-2 text-xl sm:text-2xl">
+		<span class="iconify hugeicons--alert-02"></span>
+		<h1 class="flex-1">Failed to fetch the dataset</h1>
+		<Button
+			size="sm"
+			class="variant-ghost-warning size-8"
+			onclick={() => {
+				fetchDataset($page.params.datasetId);
+			}}
+		>
+			<span class="iconify size-5 hugeicons--refresh"></span>
+		</Button>
+	</div>
+	<p class="text-muted-foreground">{datasetPageState.error}</p>
+</div>
---
+++ src/lib/components/datasets/DatasetFullCardLoading.svelte
@@ -0,0 +1,32 @@
+<script>
+	import Divider from '../shared/Divider.svelte';
+</script>
+
+<div class="card flex w-full flex-col flex-wrap gap-3 px-3 py-4 lg:flex-nowrap">
+	<div class="flex flex-wrap justify-between gap-3">
+		<div class="flex-1 space-y-2">
+			<div class="flex items-center gap-2">
+				<span class="placeholder size-8"></span>
+				<span class="placeholder h-8 w-[75%] sm:w-[65%]"></span>
+			</div>
+			<span class="placeholder h-5 w-full"></span>
+		</div>
+		<div class="flex gap-2">
+			<span class="placeholder size-7 sm:size-8"></span>
+			<span class="placeholder size-7 sm:size-8"></span>
+		</div>
+	</div>
+	<Divider />
+	<div class="flex items-center gap-3 font-medium">
+		<span class="placeholder size-7"></span>
+		<span class="placeholder h-5 w-28"></span>
+		<span class="placeholder h-6 w-8 rounded-full"></span>
+		<span class="placeholder ms-auto size-7 sm:h-8 sm:w-36"></span>
+	</div>
+	<Divider />
+	<div class="flex items-center gap-3">
+		<span class="placeholder size-7"></span>
+		<span class="placeholder h-5 w-28"></span>
+		<span class="placeholder h-5 w-28"></span>
+	</div>
+</div>
