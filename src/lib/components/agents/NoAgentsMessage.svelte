b353e3e338e5647eac01cd42bcbcfa30a8a9a6f5
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-06-19 16:54:40 +0300

    Refactor `NoAgentsMessage` component to make it cleaner

--- src/lib/components/agents/NoAgentsMessage.svelte
+++ src/lib/components/agents/NoAgentsMessage.svelte
@@ -1,8 +1,9 @@
 <div class="flex flex-1 flex-col items-center justify-center gap-3 px-4 py-20">
-	<span class="iconify size-20 hugeicons--sad-02"></span>
+	<span class="iconify size-20 hugeicons--bot"></span>
 
-	<h2 class="h2 font-bold">You dont have agents yet</h2>
+	<h2 class="h2 font-bold">You don't have agents !</h2>
 	<p class="text-center sm:text-lg">
-		You can click on the button below to go to agents creation form and create your first agent
+		You can click on "Create Agent" button on the top right to go to agents form and create your
+		first agent.
 	</p>
 </div>
