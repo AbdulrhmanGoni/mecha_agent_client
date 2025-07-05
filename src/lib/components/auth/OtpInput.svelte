d5c90e5ed14c76358561e79a3f9ca6bc55349703
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-07-06 01:39:40 +0300

    Add email verification by OTP step to sign in page
    Create `OtpInput` component to send a one time password (OTP)
    to the entered email by the user and ask him to get that OTP so
    we verify that he owns the email.

---
+++ src/lib/components/auth/OtpInput.svelte
@@ -0,0 +1,96 @@
+<script lang="ts">
+	import clientFetchAPI from '$lib/functions/clientFetchAPI';
+	import { onMount } from 'svelte';
+	import Button from '../shared/Button.svelte';
+	import LoadingSpinner from '../shared/LoadingSpinner.svelte';
+	import { getToastStore } from '@skeletonlabs/skeleton';
+	import Alert from '../shared/Alert.svelte';
+
+	let { email, onVerify }: { email: string; onVerify: () => void } = $props();
+
+	let loading = $state(false);
+	let otpSignature = $state('');
+	let sendOtpError = $state('');
+
+	const toastStore = getToastStore();
+
+	function requestOTP() {
+		loading = true;
+		clientFetchAPI<string>({ path: `/api/verify-email?email=${email}` })
+			.then((signature) => {
+				otpSignature = signature;
+			})
+			.catch((error) => {
+				sendOtpError = error;
+			})
+			.finally(() => {
+				loading = false;
+			});
+	}
+
+	onMount(requestOTP);
+
+	function onSubmit(e: SubmitFormEvent) {
+		e.preventDefault();
+
+		const formData = new FormData(e.currentTarget);
+		const otp = formData.entries().reduce((otp, input) => otp + input[1], '');
+
+		loading = true;
+		clientFetchAPI<boolean>({
+			path: `/api/verify-email?email=${email}&otp=${otp}&signature=${otpSignature}`,
+			method: 'POST'
+		})
+			.then((res) => {
+				if (res) {
+					onVerify();
+				}
+			})
+			.catch((error) => {
+				toastStore.trigger({
+					message: error,
+					background: 'variant-filled-error'
+				});
+			})
+			.finally(() => {
+				loading = false;
+			});
+	}
+</script>
+
+<form class="flex h-60 flex-col justify-center" onsubmit={onSubmit}>
+	{#if loading}
+		<LoadingSpinner className="size-14" />
+	{:else if sendOtpError}
+		<Alert
+			title="OTP Request Failed"
+			variant="error"
+			message={sendOtpError}
+			actionIconName="hugeicons--refresh"
+			actionFunction={requestOTP}
+		/>
+	{:else if otpSignature}
+		<h1 class="text-center text-lg font-semibold">Enter the OTP sent to your email</h1>
+		<div class="my-5 flex gap-3">
+			{#each [1, 2, 3, 4, 5, 6] as inputNumber}
+				<!-- svelte-ignore a11y_autofocus -->
+				<input
+					id="otp-field-{inputNumber}"
+					name="otp-field-{inputNumber}"
+					class="input variant-form-material size-12 text-center"
+					type="text"
+					pattern="\d*"
+					required
+					maxlength="1"
+					autofocus={inputNumber === 1}
+					oninput={(e) => {
+						e.currentTarget.value = e.currentTarget.value.trim();
+						e.currentTarget.value &&
+							document.getElementById(`otp-field-${inputNumber + 1}`)?.focus();
+					}}
+				/>
+			{/each}
+		</div>
+		<Button class="variant-filled-primary mx-auto" type="submit">Send</Button>
+	{/if}
+</form>
--- src/lib/components/auth/UserCredentialsForm.svelte
+++ src/lib/components/auth/UserCredentialsForm.svelte
@@ -1,83 +1,109 @@
 <script lang="ts">
 	import { signIn } from '@auth/sveltekit/client';
 	import Logo from '../Logo.svelte';
+	import OtpInput from './OtpInput.svelte';
 
 	type UserCredentialsFormProps = {
 		isNewUser: boolean;
 	};
 
+	type NewUser = {
+		username: string;
+		email: string;
+		password: string;
+	};
+
 	const { isNewUser }: UserCredentialsFormProps = $props();
 
-	function submitHandler(event: SubmitFormEvent) {
-		event.preventDefault();
-		const formData = new FormData(event.currentTarget);
+	let newUser: NewUser | undefined = $state(undefined);
+	let openOtpFormFor: string | undefined = $state();
 
-		signIn('credentials', {
-			username: formData.get('username'),
-			email: formData.get('email'),
-			password: formData.get('password')
-		});
+	function submitHandler(e: SubmitFormEvent) {
+		e.preventDefault();
+		const formData = new FormData(e.currentTarget);
+
+		const email = formData.get('email')?.toString()!;
+
+		newUser = {
+			username: formData.get('username') as string,
+			email,
+			password: formData.get('password') as string
+		};
+
+		if (!isNewUser) {
+			return signIn('credentials', newUser);
+		}
+
+		openOtpFormFor = email;
+	}
+
+	function submitCredentials() {
+		newUser && signIn('credentials', newUser);
 	}
 </script>
 
-<div class="flex w-full flex-col items-center space-y-3">
-	<Logo className="w-24" />
-	<h1 class="text-2xl font-bold">
-		Sign
-		{#if isNewUser}
-			up
-		{:else}
-			in
-		{/if}
-		<span class="text-primary-500">Mecha Agent</span>
-	</h1>
-	<form id="user-credentials-form" class="flex w-full flex-col gap-3" onsubmit={submitHandler}>
-		{#if isNewUser}
-			<label class="label">
-				<span>Username</span>
-				<input
-					class="input variant-form-material"
-					name="username"
-					type="text"
-					placeholder="Enter your name"
-					required
-					minlength="3"
-					maxlength="80"
-				/>
-			</label>
-		{/if}
-
-		<label class="label">
-			<span>Email</span>
-			<input
-				class="input variant-form-material"
-				name="email"
-				type="email"
-				placeholder="Enter your email"
-				required
-			/>
-		</label>
-
-		<label class="label">
-			<span>Password</span>
-			<input
-				class="input variant-form-material"
-				name="password"
-				type="password"
-				placeholder="Enter your password"
-				required
-				minlength="6"
-				maxlength="200"
-			/>
-		</label>
-
-		<button class="btn self-end bg-primary-500 text-white" type="submit">
+{#if isNewUser && openOtpFormFor}
+	<OtpInput email={openOtpFormFor} onVerify={submitCredentials} />
+{:else}
+	<div class="flex w-full flex-col items-center space-y-3">
+		<Logo className="w-24" />
+		<h1 class="text-2xl font-bold">
 			Sign
 			{#if isNewUser}
 				up
 			{:else}
 				in
 			{/if}
-		</button>
-	</form>
-</div>
+			<span class="text-primary-500">Mecha Agent</span>
+		</h1>
+		<form id="user-credentials-form" class="flex w-full flex-col gap-3" onsubmit={submitHandler}>
+			{#if isNewUser}
+				<label class="label">
+					<span>Username</span>
+					<input
+						class="input variant-form-material"
+						name="username"
+						type="text"
+						placeholder="Enter your name"
+						required
+						minlength="3"
+						maxlength="80"
+					/>
+				</label>
+			{/if}
+
+			<label class="label">
+				<span>Email</span>
+				<input
+					class="input variant-form-material"
+					name="email"
+					type="email"
+					placeholder="Enter your email"
+					required
+				/>
+			</label>
+
+			<label class="label">
+				<span>Password</span>
+				<input
+					class="input variant-form-material"
+					name="password"
+					type="password"
+					placeholder="Enter your password"
+					required
+					minlength="6"
+					maxlength="200"
+				/>
+			</label>
+
+			<button class="btn self-end bg-primary-500 text-white" type="submit">
+				Sign
+				{#if isNewUser}
+					up
+				{:else}
+					in
+				{/if}
+			</button>
+		</form>
+	</div>
+{/if}
