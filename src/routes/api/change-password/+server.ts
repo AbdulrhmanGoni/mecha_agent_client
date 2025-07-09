0baf5110565793e614b909dbcec0460b522ede4e
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-07-09 22:48:17 +0300

    Create "change/reset password" API routes

---
+++ src/routes/api/change-password/+server.ts
@@ -0,0 +1,13 @@
+import serverFetchAPI from "$lib/functions/serverFetchAPI.js";
+
+export async function POST({ request, cookies }) {
+    const response = await serverFetchAPI({
+        path: "/api/auth/change-password",
+        method: "POST",
+        cookies,
+        body: await request.text(),
+        headers: { "Content-Type": "application/json" }
+    });
+
+    return new Response(response.body, { status: response.status });
+}
