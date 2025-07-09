fb9628550ecb1b68e2cef258bd260e9b086e5452
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-07-09 23:38:08 +0300

    Change user accout permissions to only "sudo" permission

--- src/lib/constants/auth.ts
+++ src/lib/constants/auth.ts
@@ -1,5 +1,5 @@
 export const permissions = ["read", "write", "inference"] as const;
 export type Permission = typeof permissions[number];
 export const signatureAlgorithm = "HS512";
-export const userAccoutPermissions = permissions;
+export const userAccoutPermissions = ["sudo"];
 export const expirationTime = "60d";
