f902ee4a0467969ac337d4d3a6c0506e95623014
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-07-13 15:42:34 +0300

    Fix "user's data isn't fetched" in plans page

--- src/routes/plans/+page.server.ts
+++ src/routes/plans/+page.server.ts
@@ -1,11 +1,14 @@
 import serverFetchAPI from '$lib/functions/serverFetchAPI.js';
 
-export const load = async ({ cookies }) => {
+export const load = async ({ cookies, locals }) => {
+    const session = await locals.auth();
+
     const res = await serverFetchAPI({ path: "/api/subscriptions/plans", cookies })
         .then(res => res.json())
 
     return {
         plans: res.result,
-        error: res.error
+        session: session,
+        error: res.error,
     }
 };
