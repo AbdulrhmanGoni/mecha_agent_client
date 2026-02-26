55ed4e8f2df3b2786f1bd118448d6b9d343aef25
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2026-02-26 03:58:08 +0300

    Hotfix jwt's signature mismatch and auto logout users

--- src/routes/api/users/+server.ts
+++ src/routes/api/users/+server.ts
@@ -3,7 +3,13 @@ import serverFetchAPI from "$lib/functions/serverFetchAPI.js";
 export async function GET({ cookies }) {
     const response = await serverFetchAPI({ path: "/api/users", cookies })
 
-    return new Response(response.body, { status: response.status });
+    const json = await response.json();
+    if (json.error == "The jwt's signature does not match the verification signature.") {
+        cookies.delete("authjs.session-token", { path: "/" });
+        cookies.delete("__Secure-authjs.session-token", { path: "/" });
+    }
+
+    return new Response(JSON.stringify(json), { status: response.status });
 }
 
 export async function DELETE({ cookies }) {
